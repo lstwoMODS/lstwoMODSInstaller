dfbb98ca4242b4c5de33da94fec5521d6f5b1fbd
lstwo <92387799+lstwo@users.noreply.github.com>
2025-02-05 20:55:08 +0100

    Create ExtractToGameRoot.bat

---
+++ data/batch/ExtractToGameRoot.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+
+set "ZIP_FILE=%~1"
+set "GAME_FOLDER=%~2"
+
+powershell -Command "Expand-Archive -Path \"%ZIP_FILE%\" -DestinationPath \"%GAME_FOLDER%\" -Force"
+
+del "%ZIP_FILE%"
+
+exit /b
