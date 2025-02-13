1dfa0827403b7b202fb791865a0ed2bbb621ec4b
lstwo <92387799+lstwo@users.noreply.github.com>
2025-02-13 12:59:06 +0100

    Create MoveToPlugins.bat

---
+++ data/batch/MoveToPlugins.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+
+set "SOURCE_FILE=%~1"
+set "DEST_FOLDER=%~2\BepInEx\plugins"
+
+if not exist "%DEST_FOLDER%" mkdir "%DEST_FOLDER%"
+
+move /Y "%SOURCE_FILE%" "%DEST_FOLDER%"
+
+exit /b
