8458d5d19a7780a0d3c818b19df3cba3d34a3edc
lstwo <92387799+lstwo@users.noreply.github.com>
2025-02-13 12:59:50 +0100

    Create ExtractToBepInEx.bat

---
+++ data/batch/ExtractToBepInEx.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+
+set "ZIP_FILE=%~1"
+set "DEST_FOLDER=%~2\BepInEx"
+
+powershell -Command "Expand-Archive -Path \"%ZIP_FILE%\" -DestinationPath \"%DEST_FOLDER%\" -Force"
+
+del "%ZIP_FILE%"
+
+exit /b
