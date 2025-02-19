767dd3d5e9ca112ec9edd98e10bab8f1a6b8229f
LeShaDDoW <leshaddow3@gmail.com>
2025-02-19 13:14:42 +0100

    fixed batch files maybe

--- data/batch/ExtractToGameRoot.bat
+++ data/batch/ExtractToGameRoot.bat
@@ -7,3 +7,5 @@ set "GAME_FOLDER=%~2"
 powershell -Command "Expand-Archive -Path \"%ZIP_FILE%\" -DestinationPath \"%GAME_FOLDER%\" -Force"
 
 del "%ZIP_FILE%"
+
+exit /b
--- data/batch/MoveToPlugins.bat
+++ data/batch/MoveToPlugins.bat
@@ -6,4 +6,8 @@ set "DEST_FOLDER=%~2\BepInEx\plugins"
 
 if not exist "%DEST_FOLDER%" mkdir "%DEST_FOLDER%"
 
-move /Y "%SOURCE_FILE%" "%DEST_FOLDER%"
+copy /Y "%SOURCE_FILE%" "%DEST_FOLDER%"
+
+del "%SOURCE_FILE%"
+
+exit /b
