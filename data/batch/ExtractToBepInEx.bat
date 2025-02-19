4960bac0f1a2ca4ce3bd8fd529cb3738c5d0712e
LeShaDDoW <leshaddow3@gmail.com>
2025-02-19 13:10:15 +0100

    removed exit for debugging

--- data/batch/ExtractToBepInEx.bat
+++ data/batch/ExtractToBepInEx.bat
@@ -7,5 +7,3 @@ set "DEST_FOLDER=%~2\BepInEx"
 powershell -Command "Expand-Archive -Path \"%ZIP_FILE%\" -DestinationPath \"%DEST_FOLDER%\" -Force"
 
 del "%ZIP_FILE%"
-
-exit /b
--- data/batch/MoveToPlugins.bat
+++ data/batch/MoveToPlugins.bat
@@ -7,5 +7,3 @@ set "DEST_FOLDER=%~2\BepInEx\plugins"
 if not exist "%DEST_FOLDER%" mkdir "%DEST_FOLDER%"
 
 move /Y "%SOURCE_FILE%" "%DEST_FOLDER%"
-
-exit /b
