6f5e291160815b38cbcb530f4dc6333a77e3be86
LeShaDDoW <leshaddow3@gmail.com>
2025-06-01 12:58:36 +0200

    Update ExtractToGameRoot.bat

--- data/batch/ExtractToGameRoot.bat
+++ data/batch/ExtractToGameRoot.bat
@@ -4,7 +4,8 @@ setlocal
 set "ZIP_FILE=%~1"
 set "GAME_FOLDER=%~2"
 
-powershell -Command "Expand-Archive -Path \"%ZIP_FILE%\" -DestinationPath \"%GAME_FOLDER%\" -Force"
+powershell -NoProfile -Command ^
+    "Expand-Archive -LiteralPath '%ZIP_FILE%' -DestinationPath '%GAME_FOLDER%' -Force"
 
 del "%ZIP_FILE%"
 
